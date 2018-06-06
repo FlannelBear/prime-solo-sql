857f89778e3db070d9d0396cd3a30b9003cb2b5a
Flannel Bear <josh.flannelbear@gmail.com>
2018-06-06 16:22:53 -0500

    finished questions 3 through 5

--- database.sql
+++ database.sql
@@ -10,11 +10,15 @@ SELECT * FROM accounts WHERE username ILIKE '%a%';
 -- Update all records with an account balance of 0.00 and a transactions_attempted of 0. 
 -- Give them a new account balance of 10.00.
 
-
+UPDATE accounts SET account_balance = 10 WHERE account_balance = 0 AND transactions_attempted = 0;a
 
 -- Select all users that have attempted 9 or more transactions.
 
+SELECT * FROM accounts WHERE transactions_attempted >= 9;
+
 -- Get the username and account balance of the 3 users with the highest balances, sort highest to lowest balance. NOTE: Research LIMIT
+
+SELECT username, account_balance FROM accounts ORDER BY account_balance DESC LIMIT 3;
 
 -- Get the username and account balance of the 3 users with the lowest balances, sort lowest to highest balance.
 
